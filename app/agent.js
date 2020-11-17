91d333c9f91a24d9376e7a1758c497eeb7685ffd
ericw <ericw@marketamerica.com>
2020-11-17 13:37:07 -0800

    change issue credential to use local agent and add webhook url

--- app/agent.js
+++ app/agent.js
@@ -1,9 +1,12 @@
 import {exec} from 'child_process';
+import axios from 'axios';
 
 const DEFAULT_INTERNAL_HOST = "127.0.0.1";
 const DEFAULT_EXTERNAL_HOST = "localhost";
 const HTTP_PORT = 8060;
 const ADMIN_PORT = 8061;
+const WEBHOOK_PORT = 8062;
+const LEDGER_URL = process.env.LEDGER_URL || `http://${process.env.DOCKERHOST}:9000`
 
 function start_agent() {
     console.log('start agent');
@@ -13,27 +16,54 @@ function start_agent() {
           console.log('Error code: '+error.code);
           console.log('Signal received: '+error.signal);
         }
-        console.log('Child Process STDOUT: '+stdout);
-        console.log('Child Process STDERR: '+stderr);
     });
 
+    console.log('Child Process STDOUT: '+ agent.stdout);
+
+    //TODO:Eric for now wait 5s to allow agent startup, needs figure out how to get signal from the child process
+    new Promise((resolve, reject) => {
+        setTimeout(resolve, 5000);    
+    }).then(() => {
+        return issue_credential(); 
+    })
+
     return agent;
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+async function issue_credential() {
+    let data = {"alias": 'Repo.Agent', "seed": "my_seed_00000000000000000000" + getRandomInt(9999), "role": "TRUST_ANCHOR"};
+    axios
+    .post(`http://${DEFAULT_INTERNAL_HOST}:${ADMIN_PORT}/wallet/did/create`)
+    .then(res => {
+        console.log('call agent to issue credidential');
+        console.log(res.data);
+        create_invitation();
+
+    })
+    .catch(error => {
+        console.error(error);
+    })
 }
 
 function get_agent_args() {
     let wallet_name = 'Repo.Agent' + Math.random();
     let args = {
-        '--endpoint': `http://${DEFAULT_EXTERNAL_HOST}:{HTTP_PORT}`,
+        '--endpoint': `http://${DEFAULT_EXTERNAL_HOST}:${HTTP_PORT}`,
         '--label': 'Repo.Agent',
         '--admin-insecure-mode': '',
         '--inbound-transport': 'http 0.0.0.0 8060',
         '--outbound-transport': 'http ',
         '--admin': '0.0.0.0 8061',
+        '--webhook-url': `http://${DEFAULT_EXTERNAL_HOST}:${WEBHOOK_PORT}/webhooks`,
         '--wallet-type': 'indy', //use indy wallet for now
         '--wallet-name': wallet_name,
         '--wallet-key': wallet_name
     };
-
+    console.log(`webhook-url: http://${DEFAULT_EXTERNAL_HOST}:${WEBHOOK_PORT}/webhooks`)
     let str_arg = '';
     for (let key in args) {
         str_arg += key + ' ' + args[key] + ' ';
@@ -41,6 +71,16 @@ function get_agent_args() {
     return str_arg;
 }
 
+async function create_invitation() {
+    axios.post(`http://${DEFAULT_INTERNAL_HOST}:${ADMIN_PORT}/connections/create-invitation?auto_accept=true&multi_use=true`)
+        .then(res => {
+            console.log(res.data)
+        }).catch(error => {
+            console.error(error);
+        })
+}
+
 export default {
-    start_agent: start_agent
+    start_agent: start_agent,
+    create_invitation: create_invitation
 }
--- app/app.js
+++ app/app.js
@@ -4,7 +4,6 @@ import axios from 'axios'
 
 const app = express()
 const port = process.env.PORT || 3000
-const LEDGER_URL = process.env.LEDGER_URL || `http://${process.env.DOCKERHOST}:9000`
 
 app.get('/register', (request, response) => {
   let data = {"alias": 'Repo.Agent', "seed": "my_seed_00000000000000000000" + getRandomInt(9999), "role": "TRUST_ANCHOR"};
@@ -13,6 +12,7 @@ app.get('/register', (request, response) => {
   .then(res => {
     console.log(res);
     response.json(res.data);
+
   })
   .catch(error => {
     console.error(error.response);
@@ -24,13 +24,40 @@ app.get('/health_check', (req, res) => {
   res.send("up");
 });
 
-await agent.start_agent();
+//
+// schema fix
+//
+app.post("/schema_definition", (request, response) => {
+  console.log('schemas')
+  var data = {
+    schema_name: "vsw schema",
+    schema_version: "0.2",
+    attributes: ["name", "url", "digest", "timestamp"],
+  };
+  var config = {
+    method: "post",
+    url: `${REPO_AGENT}/schemas`,
+    headers: {},
+    data: data,
+  };
+  axios(config)
+    .then(function (res) {
+      console.log("respose received");
+      console.log(res);
+      var schema_id = res.data.schema_id;
+      console.log(res.data);
+      response.json(res.data);
+    })
+    .catch((error) => {
+      console.error(error.response);
+      response.status(500).send(error).end();
+    });
+});
+
+agent.start_agent();
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
-  console.log('ledger default url: ' + LEDGER_URL);
 });
 
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
