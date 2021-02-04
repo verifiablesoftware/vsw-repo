b028359a1022dd00c62f1b548b144cb362ef558e
Felix <gang4.chen@symbio.com>
2021-02-04 23:01:05 +0800

    switch to python branch

--- vsw_repo/commands/start.py
+++ vsw_repo/commands/start.py
@@ -1,15 +1,10 @@
-import asyncio
 import configparser
-import multiprocessing
 import os
-import sys
 import uuid
 import daemon
-from vsw_repo.controller import webhook
 from os.path import expanduser
 from pathlib import Path
 from typing import List
-from vsw_repo.controller import server
 
 from aries_cloudagent_vsw.commands import run_command
 
@@ -19,53 +14,47 @@ from vsw_repo.log import Log
 logger = Log(__name__).logger
 
 def main(args: List[str]) -> bool:
-    p1 = multiprocessing.Process(name='p1', target=server.start())
-    p1.start()
-    # p = multiprocessing.Process(name='p', target=webhook.listen_webhooks())
-
-    # p.start()
-
     start_agent()
 
 
 def start_agent():
     configuration = utils.get_vsw_repo()
 
-    with daemon.DaemonContext(stdout=sys.stdout, stderr=sys.stderr):
-        wallet_name = configuration.get("wallet_name")
-        wallet_key = configuration.get("wallet_key")
-        config_path = Path(__file__).parent.parent.joinpath("conf/genesis.txt").resolve()
-        logger.info('genesis_file: ' + str(config_path))
-        run_command('start', ['--admin', configuration.get("admin_host"), configuration.get("admin_port"),
-                      '--inbound-transport', configuration.get("inbound_transport_protocol"),
-                      configuration.get("inbound_transport_host"), configuration.get("inbound_transport_port"),
-                      '--outbound-transport', configuration.get('outbound_transport_protocol'),
-                      '--endpoint', configuration.get("endpoint"),
-                      '--label', configuration.get("label"),
-                      '--seed', get_seed(wallet_name),
-                      '--genesis-file', str(config_path),
-                      '--webhook-url', configuration.get("webhook_url"),
-                      '--accept-taa', '1',
-                      '--wallet-type', 'indy',
-                      '--wallet-name', wallet_name,
-                      '--wallet-key', wallet_key,
-                      '--public-invites',
-                      '--debug',
-                      '--log-config', logger.aries_config_path,
-                      '--log-file', logger.aries_log_file,
-                      '--debug-credentials',
-                      '--auto-accept-invites',
-                      '--auto-accept-requests',
-                      '--auto-ping-connection',
-                      '--auto-respond-messages',
-                      '--auto-respond-credential-proposal',
-                      '--auto-respond-credential-offer',
-                      '--auto-respond-credential-request',
-                      '--auto-store-credential',
-                      '--auto-respond-presentation-request',
-                      '--auto-verify-presentation',
-                      '--admin-insecure-mode'])
-
+    # with daemon.DaemonContext(stdout=sys.stdout, stderr=sys.stderr, files_preserve=logger.streams):
+    wallet_name = configuration.get("wallet_name")
+    wallet_key = configuration.get("wallet_key")
+    config_path = Path(__file__).parent.parent.joinpath("conf/genesis.txt").resolve()
+    logger.info('genesis_file: ' + str(config_path))
+    run_command('start', ['--admin', configuration.get("admin_host"), configuration.get("admin_port"),
+                  '--inbound-transport', configuration.get("inbound_transport_protocol"),
+                  configuration.get("inbound_transport_host"), configuration.get("inbound_transport_port"),
+                  '--outbound-transport', configuration.get('outbound_transport_protocol'),
+                  '--endpoint', configuration.get("endpoint"),
+                  '--label', configuration.get("label"),
+                  '--seed', get_seed(wallet_name),
+                  '--genesis-file', str(config_path),
+                  '--webhook-url', configuration.get("webhook_url"),
+                  '--accept-taa', '1',
+                  '--wallet-type', 'indy',
+                  '--wallet-name', wallet_name,
+                  '--wallet-key', wallet_key,
+                  '--public-invites',
+                  '--debug',
+                  '--log-config', logger.aries_config_path,
+                  '--log-file', logger.aries_log_file,
+                  '--debug-credentials',
+                  '--auto-accept-invites',
+                  '--auto-accept-requests',
+                  '--auto-ping-connection',
+                  '--auto-respond-messages',
+                  '--auto-respond-credential-proposal',
+                  '--auto-respond-credential-offer',
+                  '--auto-respond-credential-request',
+                  '--auto-store-credential',
+                  '--auto-respond-presentation-request',
+                  '--auto-respond-presentation-proposal',
+                  '--auto-verify-presentation',
+                  '--admin-insecure-mode'])
 
 
 def get_seed(wallet_name):
