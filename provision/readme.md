36e574f316aba1623472b14d7262a4f8e84cca9d
juha tervo <jutervo@outlook.com>
2021-02-08 14:21:41 +0200

    provision and sovrin ledger registration wallet data changed

--- provision/readme.md
+++ provision/readme.md
@@ -1,9 +1,1 @@
-## trial with aca-py image downloaded from docker hub
-Dockerfile.image
-./start_aca_image.sh
-
-## trial with installing aca-py and running that in container
-Dockerfile
-./start_aca-py.sh
-
-[hint site](https://stackoverflow.com/questions/65318621/how-to-pass-command-line-argument-as-env-variable-in-docker-run)
+# trial with provision aries cloud agent provisioning
--- repo-provision.sh
+++ repo-provision.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 # 1 - agent provision first
 
-PROVISION=provision
+PROVISION=vsw-repo-provision
 
 export SEED=my_seed_100000000000000000007777 
-export ENDPOINT=http://192.168.65.3:8060
+export ENDPOINT=http://ec2-3-138-121-46.us-east-2.compute.amazonaws.com:8060 
 export WALLET_NAME=Repo.Agent
 export WALLET_KEY=Repo.Agent 
 
 echo "start ${PROVISION}" 
+echo "SEED ${SEED}"
+echo "ENDPOINT ${ENDPOINT}"
 
 if [ !"$(docker ps -qq -f name=${PROISION})" ]; then
     echo "container ${PROVISION} is running, delete it first"
@@ -17,11 +19,8 @@ fi
 
 cd ./provision
 
-docker build -f ./Dockerfile.image \
-    -t ${PROVISION} .
-docker run -it --name ${PROVISION}\
- -p 8061:8061\
- -p 8060:8060\ 
- -e SEED_VAL=${SEED} ${PROVISION}
+docker build -f ./Dockerfile.image -t ${PROVISION} .
+
+docker run -e "SEED_VAL=my_seed_100000000000000000007777" -e "ENDPOINT=http://ec2-3-138-121-46.us-east-2.compute.amazonaws.com:8060" -it ${PROVISION}
 
 cd ..
