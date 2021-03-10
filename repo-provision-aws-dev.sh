296a84c8419c52deaf68118a478ad86f16cd82af
Juha Tervo <jutervo@outlook.com>
2021-03-10 14:27:57 +0200

    dev and master changes to docker paths

--- repo-provision-aws-dev.sh
+++ repo-provision-aws-dev.sh
@@ -1,8 +1,8 @@
 
 aca-py provision \
---endpoint http://3.141.51.96:8060 \
+--endpoint http://3.141.51.96:8070 \
 --genesis-file "/home/jte/git/vsw-repo/app/resources/genesis.txt" \
 --wallet-type indy \
 --wallet-name Repo.Agent \
 --wallet-key Repo.Agent \
---seed my_seed_000000000000000000008876
+--seed my_seed_000000000000000000007777
