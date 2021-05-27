49fdb2ca8fead5c30f0672ef97a0147046c4e8d4
Juha Tervo <jutervo@outlook.com>
2021-05-27 08:30:08 +0300

    cahnges

--- app/app.js
+++ app/app.js
@@ -29,10 +29,6 @@ const swaggerDocs = swaggerJSDoc(swaggerOptions);
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -40,7 +36,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-//app.use('/users', usersRouter);
+
 app.use('/webhooks', webhooksRouter);
 app.use('/admin', adminRouter);
 app.use('/controller', controllerRouter);
